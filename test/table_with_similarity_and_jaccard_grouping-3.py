be79174a5b659807478ab3112d8658952cb0b645
Stanojoski02 <97593843+Stanojoski02@users.noreply.github.com>
2022-09-07 13:55:34 +0200

    Update table_with_similarity_and_jaccard_grouping-3.py

--- test/table_with_similarity_and_jaccard_grouping-3.py
+++ test/table_with_similarity_and_jaccard_grouping-3.py
@@ -76,48 +76,6 @@ def jaccard_similarity(x, y):
     union_cardinality = len(set.union(*[set(x), set(y)]))
     return intersection_cardinality / float(union_cardinality)
 
-
-def from_to_excel(input_, output_):
-    with open(input_, "r", encoding="charmap") as d:
-        data = d.readlines()
-        index = 0
-        db = pandas.DataFrame({
-            "email_index": [index],
-            "from": [data[0].split(",")[1]],
-            "to": [" ".join(re.findall(r'[\w\.-]+@[\w\.-]+', data[0].split(",")[2])).lower().replace(" ", "")],
-            "date_time": [dateparser.parse(data[0].split(",")[0])]
-        })
-        writer = pandas.ExcelWriter(output_, engine='xlsxwriter')
-        db = db[["email_index", "from", "to", "date_time"]]
-
-        for email in data:
-            try:
-                em_to = " ".join(re.findall(r'[\w\.-]+@[\w\.-]+', email.split(",")[2])[0]).lower().replace(" ", "")
-                if "-" in em_to:
-                    em_to = em_to.split("-")[1]
-            except:
-                raise print(" ")
-
-            if email != data[0]:
-                new_db = pandas.DataFrame({
-                    "email_index": [index],
-                    "from": [", ".join(re.findall(r'[\w\.-]+@[\w\.-]+', email.split(",")[1])).lower()],
-                    "to": [em_to],
-                    "date_time": [dateparser.parse(email.split(",")[0])]
-                })
-                index += 1
-                db = pandas.concat([db, new_db], ignore_index=True, axis=0)
-                # if not ", ".join(re.findall(r'[\w\.-]+@[\w\.-]+', email.split(",")[1])).lower():
-                #     print(email.split(",")[1])
-        db.to_excel(writer, sheet_name='Sheet1', startrow=1, header=False, index=False)
-        worksheet = writer.sheets['Sheet1']
-        (max_row, max_col) = db.shape
-        column_settings = [{'header': column} for column in db.columns]
-        worksheet.add_table(0, 0, max_row, max_col - 1, {'columns': column_settings})
-        worksheet.set_column(0, max_col - 1, 12)
-        writer.save()
-
-
 def from_to_excel(input_, output_):
     with open(input_, "r", encoding="charmap") as d:
         data = d.readlines()
@@ -313,4 +271,4 @@ def final_function(input_file):
     similarity_sorter("sentence_with_email_index1.txt", "sorted_sentence.txt")
     create_table_with_sentence("sorted_sentence.txt", "table_with_sentence_and_similarity22.xlsx")
     from_to_excel(input_file, "from_to_table.xlsx")
-    
+final_function("emails2.txt")
