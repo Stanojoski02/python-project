1a5eab751912ed4db8d69ea8f0eeb394f065cd70
Stanojoski02 <97593843+Stanojoski02@users.noreply.github.com>
2022-09-27 23:16:31 +0200

    Delete jaccard_similarity_function.py

--- test/jaccard_similarity_function.py
+++
@@ -1,7 +0,0 @@
-import numpy
-
-def jaccard_similarity(x, y):
-    """ returns the jaccard similarity between two lists """
-    intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
-    union_cardinality = len(set.union(*[set(x), set(y)]))
-    return intersection_cardinality / float(union_cardinality)
