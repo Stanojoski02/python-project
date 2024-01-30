41ae44935d0a22e7ae7d549f2680807ee956fc62
Stanojoski02 <97593843+Stanojoski02@users.noreply.github.com>
2024-01-31 00:38:29 +0100

    Add files via upload

---
+++ odoo-tasks/l10n_hr_test/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'l10n_hr_test',
+    'version': '16.0.1.0',
+    'author': 'l10n_hr_test',
+    'category': 'Localization',
+    'depends': ['account','web'],
+    'data': [
+        "views/account_move.xml",
+        'views/account_move_report.xml'
+    ],
+    'installable': True,
+}
